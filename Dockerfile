840179c84b17e39fb1e86cd137f732b6aa39b82f
Carl Allen <carl@allenofmn.com>
2019-04-15 14:19:33 -0500

    new repo

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM python:2.7
+MAINTAINER Carl Allen <github@allenofmn.com>
+
+
+ENV API_USER=monuser \
+    DESCRIPTION=UPS \
+    NAME=ups \
+    DRIVER=usbhid-ups \
+    PORT=auto \
+    USER=nut \
+    GROUP=nut \
+    DRIVER=usbhid-ups
+
+RUN apt-get install nut
+
+RUN mkdir /app && \
+    cd /app && \
+    git clone https://github.com/rshipp/python-nut2.git && \
+    cd python-nut2 && \
+    python setup.py install && \
+    cd .. && \
+    git clone https://github.com/rshipp/webNUT.git && cd webNUT && \
+    pip install -e .
+
+
+
+COPY /docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod +x /docker-entrypoint.sh
+
+WORKDIR /app/webNUT
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
+
+EXPOSE 6543 3493
---
+++ docker-entrypoint.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -xe
+
+upshost="${UPS_HOST:-127.0.0.1}"
+upsport="${UPS_PORT:-3493}"
+upsuser="${UPS_USER:-monuser}"
+upspassword="${UPS_PASSWORD:-secret}"
+
+if [[ ! -f /etc/nut/ups.conf ]]; then
+  cat >/etc/nut/ups.conf <<EOF
+[$NAME]
+  desc = "$DESCRIPTION"
+  driver = $DRIVER
+  port = $PORT
+EOF
+fi
+
+if [[ ! -f /etc/nut/upsd.conf ]]; then
+  cat >/etc/nut/upsd.conf <<EOF
+LISTEN 0.0.0.0 3493
+EOF
+fi
+
+if [[ ! -f /etc/nut/upsd.users ]]; then
+  cat >/etc/nut/upsd.users <<EOF
+[$upsuser]
+  password = $upspassword
+  upsmon master
+EOF
+fi
+
+if [[ ! -f /etc/nut/upsmon.conf ]]; then
+  cat >/etc/nut/upsmon.conf <<EOF
+MONITOR $NAME@localhost 1 $upsuser $upspassword master
+RUN_AS_USER $USER
+EOF
+fi
+
+echo "server = '$upshost'" > /app/webNUT/webnut/config.py
+echo "port = '$upsport'" >> /app/webNUT/webnut/config.py
+echo "username = '$upsuser'" >> /app/webNUT/webnut/config.py
+echo "password = '$upspassword'" >> /app/webNUT/webnut/config.py
+
+cat /app/webNUT/webnut/config.py
+
+cd /app/webNUT && python setup.py install
+
+chgrp -R nut /etc/nut
+chmod -R o-rwx /etc/nut
+
+/sbin/upsdrvctl -u root start
+/sbin/upsd -u $USER
+/sbin/upsmon
+
+cd webnut
+exec pserve ../production.ini
